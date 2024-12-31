8e5c51147fc91554f8c02eeb7675191cd6398455
Peralta Rocio <peralta.rocio.2229@gmail.com>
2024-12-31 19:07:10 -0300

    funcionalidad de Js de imagen usuario, inputtext arriba y abajo, mas el check de arriba y abajo

--- main.js
+++ main.js
@@ -4,96 +4,93 @@
 function $(elements) {
    return document.querySelector (elements)
 }
-// AQUI COMIENZA LAS CONST
+// AQUI COMIENZA EL TP
 
-//CONTENEDOR CENTRAL:
-//HEADER:
-const $contenedorCentral = $("#divCentral")
-const $contenedorHeader = $("#divHeader") 
-const $h1Central = $("#h1Titulo")
-const $navCentral = $("#navCentral")
-const $botonNav1 = $ ("#boton1")
-const $botonNav2 = $ ("#boton2")
-const $botonModo = $ ("#modoClaro")
-//MAIN
-const $contenedorMain = $("#divSecciones")
-const $textoTop = $("#divTop")
-const $imagenMeme = $("#img")
-const $textoBottom = $("#divBottom")
-const $descargarimg = $("#descargarMeme")
-//ASIDE:
-const $contenedorAside = $("#divAside")
-const $cierreImg = $("#botonCierre1")
-//SECCION IMAGEN
-const $opcionesImg = $("#opcionesImagen")
-const $h1Aside= $("#asideTitulo") 
-const $contenedorUrl = $(".divUrl")
-const $inputUrl = $("#urlInput")
-const $colorFondo = $("#colorFondo")
-const $inputColor = $ ("#inputColorFondo")
-const $SelectorFondo = $ ("#selectFondo")
-const $divFiltros = $ ("#divFiltros")
-const $inputBrillo = $ ("#brillo")
-const $inputOpacidad = $ ("#opacidad")
-const $inputContraste = $ ("#contraste")
-const $inputDesenfoque = $ ("#desenfoque")
-const $inputEscala = $ ("#escalaDeGrises")
-const $inputSepia = $ ("#sepia")
-const $inputHue = $ ("#hue")
-const $inputSaturado = $ ("#saturado")
-const $inputNegativo = $ ("#negativo")
-const $restablecerFiltros = $ ("#botonRestablecer")
+//MODO CLARO-MODO OSCURO
 
-//SECCION TEXTO
-const $contenedorTexto = $("#divTexto")
-const $cierreTexto = $ ("#botonCierre2")
-const $tituloTexto = $("#h1Texto")
-const $top1Texto = $ ("#TopText")
-const $textarea1 = $ ("#textareaDiv")
-const $checkTexto = $("#checkTexto")
-const $fuentes = $ ("#divFuentes")
-const $funte1 = $ ("#impact")
-const $funte2 = $ ("#arial")
-const $funte3 = $ ("#arialBlack")
-const $funte4 = $ ("#american")
-const $funte5 = $ ("#mono")
-const $funte6 = $ ("#comic")
-const $funte7 = $ ("#helvetica")
-const $funte8 = $ ("#verdana")
-const $funte9 = $ ("#times")
-const $tamañoFuente= $("#numeroFuente")
-const $alinearTexto1 = $ ("#iconosFuente1")
-const $alinearTexto2 = $ ("#iconosFuente2")
-const $alinearTexto3 = $ ("#iconosFuente3")
-const $ContenedorColores= $("#seccionColor")
-const $contenedorColores = $("#divColores")
-const $inputColores= $ ("#inputColor")
-const $contenedorFondo = $("#divFondo")
-const $inputFondo = $("#colorFondo")
-const $checkColor = $("#checkColor")
-const $contenedorContorno = $("#divContorno")
-const $boton1= $ ("#botonContorno1")
-const $boton2= $ ("#botonContorno2")
-const $boton3= $ ("#botonContorno3")
-const $contenedorEspaciado= $ ("#divEspaciado")
-const $inputNumero= $ ("#inputNumber")
-const $contenedorInterlineados= $ ("#divInterlineado")
-const $selectInterlineados = $ ("#selectInterlineado")
+const $bodymodo = $("body")
+const $botonModos = $(".botonNav")
 
-// AQUI COMIENZAN LOS EVENTOS
+$botonModos.addEventListener ("Click", () => {
+  if ($bodymodo.classList.contains("modoClaro")){
+    $bodymodo.classList.remove("modoClaro")
+    $bodymodo.classList.add("modoOscuro")
+} else {
+    $bodymodo.classList.remove("modoOscuro")
+    $bodymodo.classList.add("modoClaro")
+}
+})
+
+//SECCION IMAGEN:
+
+//imagen del usuario :
+
+const $url = $ ("#urlInput")
+const $imagenMeme = $ ("#imagenMeme")
+
+$url.addEventListener( "input", () => {
+  if ($imagenMeme.src = " " ){
+  $imagenMeme.src = $url.value;
+}
+} )
+
+//fondo color
+
+// fondo select
+
+//filtros
+
+// restablecer valores boton
+
+//SECCION TEXTO:
+//BOTON DE CERRAR LA SECCION 
+
+const $botonCierre = $("#botonCierre2")
+const $seccionTexto = $("#divTexto")
+
+$botonCierre.addEventListener ("Click", () => {
+ $seccionTexto.style.display = "none" 
+})
+
+//texto superior:
+
+const $textoArriba = $("#textoArriba")
+const $inputTextTop =$("#textareaDiv")
+
+$inputTextTop.addEventListener("input", () =>{ 
+  $textoArriba.innerText = $inputTextTop.value 
+})
+
+// INPUT DEL CHEK  SUPERIOR 
+const $checkTexto = $ ("#checkTexto")
+const $divTextoTop = $ (".contenedorTexto1")
+
+$checkTexto.addEventListener("input", () => {
+ if ($divTextoTop.style.display = "flex") {
+  $divTextoTop.style.display = "none"
+} } )
+
+// COMO LO HAGO VOLVER? -..........................................
+
+//texto inferior:
+
+const $textoAbajo = $("#textoAbajo")
+const $inputTextButtom = $("#textareaButtom")  
 
 
+$inputTextButtom.addEventListener ("input" , () => {
+  $textoAbajo.innerText = $inputTextButtom.value
+})
 
+//INPUT DEL CHECK INFERIOR
 
+const $checkInferior = $("#checkTextoInferior")
+const $divInferior = $(".contenedorTexto2")
 
+$checkInferior.addEventListener ("input" , () => {
+  $divInferior.style.display = "none"
+})
 
-//url
+//como lo hago volver? ............................................................
 
-const $divImg = document.querySelector("#img") 
-const $urlUsuario = document.querySelector ("urlInput")
-   if ($divImg = " ") {
-      $divImg.src = Url.  $urlUsuario
-   }
-
-// LAS EJECUCIONES DE FUNCIONES
-
+//FUENTE
